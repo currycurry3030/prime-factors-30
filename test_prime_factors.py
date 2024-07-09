74d54ce06ecc034b58b7a2ebdc2128fdec319749
bj93.park <qazws9346@gmail.com>
2024-07-10 08:59:52 +0900

    [feature] 25

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -6,3 +6,6 @@ class PrimeFactorTest(TestCase):
         prime_factor = PrimeFactor()
         self.assertEqual([],prime_factor.of(1))
 
+    def test_prime_factor_of_2(self):
+        prime_factor = PrimeFactor()
+        self.assertEqual([2], prime_factor.of(2))
