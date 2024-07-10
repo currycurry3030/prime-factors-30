bbc71b1fd859e544a589f674b22ebc7b9f517881
bj93.park <qazws9346@gmail.com>
2024-07-10 09:35:53 +0900

    [feature] 49

--- prime_factors.py
+++ prime_factors.py
@@ -2,7 +2,7 @@ class PrimeFactor:
     def of(self, number) -> []:
         factors = []
         if number > 1:
-            if number in [4, 6, 9]:
+            if number in [4, 6, 9, 12]:
                 divisor = 2
                 while number > 1:
                     while number % divisor == 0:
